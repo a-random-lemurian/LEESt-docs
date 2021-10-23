4b7d77916a484ae2536ccc81dc2b0ffd83c97bfa
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-10-23 16:23:50 +0800

    Random phrases now on main page

---
+++ script/js/randomphrase.js
@@ -0,0 +1,27 @@
+const Phrases = Array(
+	"The world's greatest Endless Sky plugin.",
+	"Where that one Hai ambassador reigns supreme.",
+	"The Anime Organization - archiving mankind's anime since 2133 CE",
+	"RNS Triggerhappy: A salvager's worst enemy",
+	"Best of luck to you, First Last!",
+	"I've been a textile worker on New Boston for God knows how long.",
+	"The all-new Amidala-class Battleship. Based on alien designs.",
+	"I've heard Orbona is a great place for salvage.",
+	"The SPA - letting kids sleep at night with their Sayari Plushies since God knows how long.",
+	"Illumina Regions tours, 15% off"
+)
+function getPhrase() {
+	let phraseIndex = Math.floor(Math.random() * Phrases.length);
+	document.getElementById("randphrase").innerHTML = Phrases[phraseIndex];
+};
+
+function consolelogmsgs() {
+	let incrementedPhraseIndex = 0
+	while (incrementedPhraseIndex < Phrases.length) {
+		console.log(Phrases[phraseIndex]);
+	}
+}
+function consoleLogCopyright() {
+	let CopyrightMessage = 'Dear user, this webpage is copyright (C) Lemuria 2021 and you may use this content under the terms of the GNU General Public License.'
+	console.log(CopyrightMessage)
+}
